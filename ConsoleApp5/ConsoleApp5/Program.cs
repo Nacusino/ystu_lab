b12fe43cf07b81adfb3188b2b68453584e1d87fd
Pakulev Ilya <pakulevilja@mail.ru>
2023-05-23 04:33:27 +0300

    Calculator create

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите первое число");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите операцию");
+string op = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+int second = int.Parse(Console.ReadLine());
+int ans;
+switch (op)
+{
+    case "+":
+        Console.WriteLine("Ответ: " + (first + second));
+        break;
+    case "-":
+        Console.WriteLine("Ответ: " + (first - second));
+        break;
+    case "*":
+        Console.WriteLine("Ответ: " + (first * second));
+        break;
+    case "/":
+        Console.WriteLine("Ответ: " + (first / second));
+        break;
+}
+
