e21f1987c9898fdaa485ce5022f7b2f7d396a017
Pakulev Ilya <pakulevilja@mail.ru>
2023-05-23 09:00:35 +0300

    Cycle with condition

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -1,23 +1,34 @@
-﻿Console.WriteLine("Введите первое число");
-int first = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите операцию");
-string op = Console.ReadLine();
-Console.WriteLine("Введите второе число");
-int second = int.Parse(Console.ReadLine());
-int ans;
-switch (op)
+﻿Console.WriteLine("Это приложение - простой калькулятор для математических выражений!");
+bool go = true;
+while (go)
 {
-    case "+":
-        Console.WriteLine("Ответ: " + (first + second));
-        break;
-    case "-":
-        Console.WriteLine("Ответ: " + (first - second));
-        break;
-    case "*":
-        Console.WriteLine("Ответ: " + (first * second));
-        break;
-    case "/":
+    Console.WriteLine("Введите первое число");
+    int first = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите операцию");
+    string op = Console.ReadLine();
+    Console.WriteLine("Введите второе число");
+    int second = int.Parse(Console.ReadLine());
+    int ans;
+    switch (op)
+    {
+        case "+":
+            Console.WriteLine("Ответ: " + (first + second));
+            break;
+        case "-":
+            Console.WriteLine("Ответ: " + (first - second));
+            break;
+        case "*":
+            Console.WriteLine("Ответ: " + (first * second));
+            break;
+        case "/":
             Console.WriteLine("Ответ: " + (first / second));
             break;
+    }
+    Console.WriteLine("Завершить работу программы?(Y/N)");
+    string letter = Console.ReadLine();
+    if(letter == "Y")
+    {
+        go = false;
+    }
 }
 
